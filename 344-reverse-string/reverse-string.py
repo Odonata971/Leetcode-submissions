3ab9b3052624462edcc4cd2da44b1a78d678c4a4
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-06-02 16:45:08 +0200

    Time: 172 ms (38.96%) | Memory: 20.7 MB (98.96%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,9 +3,6 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        n = len(s)
-        for i in range(n//2):
-            temp = s[i]
-            s[i] = s[n - i - 1]
-            s[n - i - 1] = temp
+        for i in range(len(s)//2):
+            s[i], s[- i - 1] = s[- i - 1], s[i]
         
