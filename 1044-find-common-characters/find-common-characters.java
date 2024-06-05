10e4a8b5c388ac224562d88a7af6c18c8cdb067a
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-06-05 13:33:14 +0200

    Time: 1 ms (100.00%) | Memory: 43.4 MB (39.59%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,26 +1,47 @@
 class Solution {
     public List<String> commonChars(String[] words) {
         
-        List<String> res = new ArrayList<>();
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            int minCount = Integer.MAX_VALUE;
-
-            for (String s : words) {
-                int count = 0;
-                for (char c : s.toCharArray()) {
-                    if (c == ch) {
-                        ++count;
-                    }
+        // Count characters in the first word
+        int[] last = count(words[0]);
+        
+        // Update the count array by intersecting with each subsequent word
+        for (int i = 1; i < words.length; i++) {
+            last = intersection(last, count(words[i]));
+        }
+        
+        // Build the result list based on the final count array
+        List<String> arr = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            if (last[i] != 0) {
+                char a = (char) ('a' + i);
+                String s = String.valueOf(a);
+                while (last[i] > 0) {
+                    arr.add(s);
+                    last[i]--;
                 }
-                minCount = Math.min(minCount, count);
-            }
-
-            for (int i = 0; i < minCount; i++) {
-                res.add(String.valueOf(ch));
             }
         }
-        return res;
+        return arr;
     }
+
+    // Calculate the intersection of two count arrays
+    private int[] intersection(int[] a, int[] b) {
+        int[] t = new int[26];
+        for (int i = 0; i < 26; i++) {
+            t[i] = Math.min(a[i], b[i]);
+        }
+        return t;
+    }
+
+    // Count the frequency of each character in a word
+    private int[] count(String str) {
+        int[] t = new int[26];
+        for (char c : str.toCharArray()) {
+            t[c - 'a']++;
+        }
+        return t;
+    }
+    
 
     /*
     public List<String> commonChars(String[] words) {
