4de74f33bfcbd53dabda7dd9655a1606e8d79f43
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-06-14 08:38:08 +0200

    Time: 646 ms (52.48%) | Memory: 30.3 MB (79.28%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i] <= nums[i - 1]:
                 res += (nums[i-1] - nums[i]) + 1
-                nums[i] += (nums[i-1] - nums[i]) + 1
+                nums[i] = nums[i-1] + 1
         return res
