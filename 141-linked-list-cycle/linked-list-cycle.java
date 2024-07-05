cd7894b7b4cc986be10d2e73a0a11f81c305f4cc
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-05 12:09:26 +0200

    Time: 0 ms (100.00%) | Memory: 44.3 MB (52.04%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -10,9 +10,12 @@
  * }
  */
 public class Solution {
+    /*
+    This is brute force (down below another approach with fast and slow pointer)
+
     public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
-        
+        if (head == null || head.next == null) return false;
+
         int gotThrough = Integer.MAX_VALUE;
         while (head.next != null) {
             if (head.next.val == gotThrough) {
@@ -20,6 +23,23 @@ public class Solution {
             }
             head.val = gotThrough;
             head = head.next;
+        }
+        return false;
+    }*/
+
+    public boolean hasCycle (ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
         }
         return false;
     }
