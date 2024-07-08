b5edd9fa32054263472282b2f4a9b65acd9a0b8f
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-08 12:46:02 +0200

    Time: 24 ms (8.02%) | Memory: 48.7 MB (97.73%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        for (Integer name: freq.keySet()) {
+            String key = name.toString();
+            String value = freq.get(name).toString();
+            System.out.println(key + " " + value);
+        }
+
+        int max = nums[0];
+        for (int key: freq.keySet()) {
+            max = freq.get(max) < freq.get(key) ? key : max;
+        }
+        return max;
+    }
+}
