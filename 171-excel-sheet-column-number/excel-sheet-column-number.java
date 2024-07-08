eae32fe9d4ad1ca5803770d618222c4ccc152df9
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-08 13:03:43 +0200

    Time: 1 ms (86.66%) | Memory: 41.8 MB (82.31%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        int index = 0;
+        for (char ch : columnTitle.toCharArray()) {
+            result *= 26;
+            result += (ch - 'A') + 1 ; 
+        }
+        return result;
+    }
+}
