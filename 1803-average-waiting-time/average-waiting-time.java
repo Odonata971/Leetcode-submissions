e1d707f1eb3e52b98becc8a72d63f979803121ee
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-09 20:55:00 +0200

    Time: 515 ms (5.31%) | Memory: 74.4 MB (9.39%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        double sum = 0.0;
+        int n = customers.length;
+        int availableAt = customers[0][0];
+        for (int i = 0; i < n; i++){
+            System.out.println("Sum: " + sum + "  arrival I: " + customers[i][0] + "  time I: " + customers[i][1] + "  availableAt:  "+availableAt);
+            sum += (availableAt - customers[i][0]) > 0
+            ?(availableAt - customers[i][0]) + customers[i][1]
+            : customers[i][1];
+            availableAt += (customers[i][0] - availableAt) > 0
+            ? (customers[i][0] - availableAt) + customers[i][1] : customers[i][1];
+        }
+        System.out.println("Sum: " + sum + "  arrival I: " + customers[n-1][0] + "  time I: " + customers[n-1][1] + "  availableAt:  "+availableAt);
+        return sum / n;
+    }
+}
