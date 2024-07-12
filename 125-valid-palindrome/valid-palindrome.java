183e3a9212769f43dae7b8275d8223492ffa14bc
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-12 15:30:03 +0200

    Time: 17 ms (18.40%) | Memory: 45.1 MB (20.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z\\d]*","").toLowerCase();
-        StringBuilder reverse = new StringBuilder(s).reverse();
-        return reverse.toString().equals(s);
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+        
     }
 }
