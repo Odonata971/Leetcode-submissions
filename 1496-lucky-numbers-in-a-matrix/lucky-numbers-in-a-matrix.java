a9f2699a5d86b11cc86f3299b4bbbe428968818c
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-07-19 09:14:54 +0200

    Time: 11 ms (5.52%) | Memory: 45.4 MB (19.38%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -3,8 +3,8 @@ class Solution {
         List<Integer> luckyNumbers = new ArrayList<>();
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (minLine(matrix[i][j], matrix[i]) && maxCol(matrix[i][j], matrix, j)) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (checkLucky(matrix[i][j], matrix, i, j)) {
                     luckyNumbers.add(matrix[i][j]);
                 }
             }
@@ -12,14 +12,21 @@ class Solution {
         return luckyNumbers;
     }
 
-    private boolean minLine(int value, int[] line) {
-        int min = line[0];
-        for (int i = 0; i < line.length; i++) {
-            if (min > line[i]) {
-                min = line[i];
+    private boolean checkLucky(int value, int[][] matrix, int i, int j) {
+        int min = matrix[i][0];
+        for (int k = 0; k < matrix[i].length; k++) {
+            if (min > matrix[i][k]) {
+                min = matrix[i][k];
             }
         }
-        return min == value;
+
+        int max = matrix[0][j];
+        for (int k = 0; k < matrix.length; k++) {
+            if (max < matrix[k][j]) {
+                max = matrix[k][j];
+            }
+        }
+        return min == value && max == value;
     }
 
     private boolean maxCol(int value, int[][] matrix, int j) {
