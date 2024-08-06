20b81dc6dc1a77f7f248b96aef740ab5a7f42756
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-08-06 20:53:33 +0200

    Time: 9 ms (61.15%) | Memory: 45.7 MB (31.73%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -4,15 +4,13 @@ class Solution {
         for (char c : word.toCharArray()) {
             freq[c - 'a']++;
         }
-        
-        Integer[] freqBoxed = Arrays.stream(freq).boxed().toArray(Integer[]::new);
-        Arrays.sort(freqBoxed, Collections.reverseOrder());
+        Arrays.sort(freq);
 
         int minPushes = 0;
         for (int i = 0; i < 26; i++) {
             int keyTaps = i / 8 + 1;
 
-            minPushes += freqBoxed[i] * keyTaps;
+            minPushes += freq[26 - i - 1] * keyTaps;
         }
 
         return minPushes;
