b8817f4ccbb9ff30f02656880b6f328c979914d6
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-09-01 11:39:27 +0200

    Time: 3 ms (97.73%) | Memory: 55.8 MB (11.21%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -4,9 +4,9 @@ class Solution {
 
         int[][] res = new int[m][n];
         int count = 0;
+
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                System.out.println(count);
                 res[i][j] = original[count++];
             }
         }
