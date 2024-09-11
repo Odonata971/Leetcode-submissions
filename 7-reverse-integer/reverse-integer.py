3580e0902f1f0f03216ff007ab0cfe4cb7866dc2
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-09-11 17:39:43 +0200

    Time: 40 ms (30.99%) | Memory: 16.5 MB (42.66%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,7 +3,7 @@ class Solution:
         neg: int = -1 if x < 0 else 1 
         maxi: int = 2 ** 31 - 1
         
-        x *= neg
+        x = abs(x)
         res = 0
         while x > 0:
             res = (res * 10) + (x % 10)
