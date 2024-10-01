03ede868eaa6778df898e609ad234fe21de09149
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-10-01 21:38:41 +0200

    Time: 4 ms (98.96%) | Memory: 57 MB (26.20%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
         int[] freq = new int[k];
-        for(int num = 0; num < arr.length; num++){
+        for (int num = 0; num < arr.length; num++) {
             int rem = arr[num] % k;
-            if(rem < 0){
+            if (rem < 0) {
                 rem = rem + k;
             }
             freq[rem]++;
         }
-        if(freq[0] % 2 != 0){
+        if (freq[0] % 2 != 0) {
             return false;
         }
-        for(int i = 1; i <= k / 2; i++){
-            if(freq[i] != freq[k - i]) {
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) {
                 return false;
             }
         }
