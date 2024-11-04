7fa546adbfda5a8a257e0880dc43f24932a018e5
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2024-11-04 13:47:22 +0100

    Time: 21 ms (54.24%) | Memory: 46 MB (45.62%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder ans = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < word.length(); ++i) {
+            if (count < 9 && word.charAt(i) == word.charAt(i - 1)) {
+                count++;
+            } else {
+                ans.append(count);
+                ans.append(word.charAt(i - 1));
+                count = 1;
+            }
+        }
+        ans.append(count);
+        ans.append(word.charAt(word.length() - 1));
+        return ans.toString();
+    }
+}
