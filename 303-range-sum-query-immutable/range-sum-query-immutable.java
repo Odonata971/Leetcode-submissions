fdc79b042cd10175018b7750ae02d9352ed30ada
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-06-20 16:16:07 +0200

    Time: 7 ms (100.00%) | Memory: 49.2 MB (94.58%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -2,11 +2,16 @@ class NumArray {
 
     int[] nums;
     public NumArray(int[] nums) {
-        this.nums = nums;
+        this.nums = new int[nums.length];
+        this.nums[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            this.nums[i] = nums[i] + this.nums[i - 1]; 
+        }
     }
     
     public int sumRange(int left, int right) {
-        return Arrays.stream(this.nums, left, right + 1).sum();
+        if(left == 0) return nums[right];
+        return nums[right] - nums[left - 1];
     }
 }
 
