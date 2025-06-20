8b751747dca43a4da1bbd7b0b5566aa50ccb8233
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-06-20 16:50:36 +0200

    Time: 1 ms (96.57%) | Memory: 42 MB (20.13%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
@@ -4,12 +4,15 @@ class Solution {
 
         if (n < 3) return 0;
         int ans = 0;
-        
+        //Keep values in a set to compare
         Set<Integer> arr = new HashSet<>();
         for (int i = 0; i < n; i++) {
             arr.add(nums[i]);
         }
+
         for (int j = 1; j < n - 1; j++) {
+            // nums[j] - nums[i] = diff <=> nums[i] = nums[j] - diff
+            // nums[k] - nums[j] = diff <=> nums[k] = nums[j] + diff
             if (arr.contains(nums[j] + diff) && arr.contains(nums[j] - diff)) ans++;
         }
         return ans;
