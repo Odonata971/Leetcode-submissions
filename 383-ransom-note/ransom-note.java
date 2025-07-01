ebf0d356d314c64256b46feb6985c9384bde7c69
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-07-01 11:57:52 +0200

    Time: 2 ms (81.38%) | Memory: 44.8 MB (66.98%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
+        if(ransomNote.length() > magazine.length()) return false;
+
         int[] freq = new int[26];
-        for (char ch : ransomNote.toCharArray()) {
+        for (char ch : magazine.toCharArray()) {
             freq[ch - 'a']++;
         }
-        for (char ch : magazine.toCharArray()) {
+        for(char ch : ransomNote.toCharArray()) {
             freq[ch - 'a']--;
-        } 
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] > 0) return false;
+            if(freq[ch - 'a'] < 0)
+                return false;
         }
         return true;
     }
