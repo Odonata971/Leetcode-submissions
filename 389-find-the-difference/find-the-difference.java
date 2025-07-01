b5aa17f9d6d342ffaf92e0f653ccebe2b754d1de
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-07-01 14:59:27 +0200

    Time: 1 ms (99.74%) | Memory: 41.8 MB (63.52%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,8 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char extraChar = 0;
+        for (char c : s.toCharArray()) extraChar ^= c;
+        for (char c : t.toCharArray()) extraChar ^= c;
+        return extraChar;
+    }
+}
