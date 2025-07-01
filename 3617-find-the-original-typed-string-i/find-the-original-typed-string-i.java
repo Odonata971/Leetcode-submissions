075097c8c0345f483efb117c638cee51a1d7d1c8
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-07-01 09:34:47 +0200

    Time: 1 ms (87.19%) | Memory: 42.1 MB (61.16%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int possibleSpellings = 0;
+        char[] charArray = word.toCharArray();
+        for (int i = 0; i < charArray.length - 1; i++) {
+            char ch = charArray[i];
+            if (charArray[i] == charArray[i + 1]) {
+                possibleSpellings++;
+            }
+        }
+        //We're adding one in case there is no misspelling
+        return possibleSpellings + 1;
+    }
+}
