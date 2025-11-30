c211306de091a90096923ca429d73bc14edd6087
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2025-11-30 16:05:47 +0100

    Time: 0 ms (100%), Space: 29.2 MB (53.97%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int ArrangeCoins(int n) {
+        // Use the formula x * (x + 1) / 2 = n => x² + x = 2n
+        // => x² + x - 2n = 0 then solve this polynomial equation 
+        // delta = 1 + 8n and positive solution = (-1 + sqrt(1 + 8n)) /2
+        // finally, we have to floor by casting to int this result to have the final complete rows number
+        return (int)((-1 + Math.Sqrt(1 + 8 * (double) n)) / 2);
+    }
+}
