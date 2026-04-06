d79bd0efe28bc4422417e8311641ef8e0b882ee1
Florian Fabre <130738441+Odonata971@users.noreply.github.com>
2026-04-06 20:08:44 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,17 +1,6 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        occurrences = [0, 0]
-        for ch in moves :
-            match ch:
-                case "L":
-                    occurrences[0] += 1
-                case "R":
-                    occurrences[0] -= 1
-                case "U":
-                    occurrences[1] += 1
-                case "D":
-                    occurrences[1] -= 1
-        return occurrences[0] == 0 and occurrences[1] == 0
+        return moves.count("L") == moves.count("R") and moves.count("U") == moves.count("D")
             
                
        
